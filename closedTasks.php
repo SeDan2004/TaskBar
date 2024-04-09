dbe07acc4e3d27c997e27984ebd7fe2cc0a626f4
SeDan2004 <sergeydanshov2004@gmail.com>
2024-04-09 11:47:00 +0300

    Add front2

---
+++ closedTasks.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <meta charset="utf-8"/>
+        <meta name="viewport" content="width=device-width;height=device-height"/>
+        <link rel="stylesheet" href="./static/closedTasks.css"/>
+    </head>
+    <body>
+        <h2 class="closed_tasks">Закрытые задачи</h2>
+
+        <div class="show_tasks">
+            <input type="date"/>
+            <input type="date"/>
+            <input type="button" value="Показать задачи"/>
+        </div>
+
+        <script src="./static/closedTasks.js"></script>
+    </body>
+</html>
